116e75979de7ff23737240896f437c0af1e22387
Remon Lam <remon@containerstack.io>
2017-07-30 23:34:38 +0200

    add shell script

---
+++ create_macos_install_disk.sh
@@ -0,0 +1,1 @@
+ sudo /Applications/Install\ macOS\ Sierra.app/Contents/Resources/createinstallmedia --volume /Volumes/Install\ macOS\ Sierra/ --applicationpath /Applications/Install\ macOS\ Sierra.app --nointeraction
